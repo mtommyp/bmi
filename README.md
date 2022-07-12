fd876b96e68fefdd5a9da692cd85ec57175921fa
Muhammad Tommy Prasetyo <57124961+mtommyp14@users.noreply.github.com>
2022-07-12 21:20:30 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+## Access  API
+
+`https://mtommyp.com/bmi`
+
+## Requirment
+
+- fastapi==0.78.0
+- pydantic==1.9.1
+- python-dotenv==0.20.0
+- uvicorn==0.18.2
+- virtualenv-clone==0.5.7
+
+---
+
+## Test
+
+- **inputs:** height(_inches_), weight(_pounds_)
+- **outputs:** BMI
+
+---
+
+## Results
+
+### FastAPI Docs:
+
+![screenshot](ss1.png)
+![screenshot](ss2.png)
---
+++ main.py
@@ -0,0 +1,25 @@
+from imp import reload
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_item(height: float, weight: float):
+    return {"BMI": bmi(height, weight),
+                 "Label": label(bmi(height, weight))}
+
+def bmi(height, weight):
+    hasil = (weight) / ((height/100)**2)
+    return hasil
+
+def label(bmilabel): 
+    if bmilabel > 24.9:
+        return "Overweight"
+    elif bmilabel < 18.5:
+        return "Underweight"
+    else:
+        return "Normal"
+
+if __name__ == "__main__":
+    Union.run('main:app', host='127.0.0.1', port=8000, log_level="info", reload=True)
